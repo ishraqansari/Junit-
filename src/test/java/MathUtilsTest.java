30a3cbb609c4f6fe7748b56920f049abc63937fe
Ishraq Ansari <ishraq.ansari@tothenew.com>
2021-06-03 13:31:09 +0530

    Initial Commit

---
+++ src/test/java/MathUtilsTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+    @Test
+    void test(){
+        System.out.println("Test running");
+    }
+}
